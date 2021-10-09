41a0c9e08797826a0d5b240496d19cfa63017529
Jared Wogan <jared.wogan@gmail.com>
2021-10-08 23:34:09 -0400

    .gitignore changes, setup.py update

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 from setuptools import setup
 
+with open("README.md", "r") as f:
+    readmetext = f.read()
+
 if __name__ == "__main__":
-    setup()
+    setup(
+        long_description=readmetext,
+        long_description_content_type="text/markdown"
+    )
